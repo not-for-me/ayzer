3071edda97fcf9d85eb43b79fc4127ea71fd1168
woojin.joe <jwj0831@gmail.com>
2017-11-22 09:01:54 +0900

    Add default server code

--- ayzer-server/server.go
+++ ayzer-server/server.go
@@ -1,17 +1,71 @@
 package main
 
+// TODO unit test
 import (
 	"encoding/json"
+	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"strconv"
+	"syscall"
 )
 
-func helloWorld(w http.ResponseWriter, r *http.Request) {
+const webRootPath = "/ayzer-web"
+const portNum = 5700
 
+func main() {
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		shutdown()
+		os.Exit(1)
+	}()
+
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fs := http.FileServer(http.Dir(dir + webRootPath))
+
+	http.Handle("/", fs)
+	http.HandleFunc("/_setting/ping", ping)
+
+	localIP := getLocalIP()
+	log.Println("Listening to " + localIP + ":" + strconv.Itoa(portNum))
+	http.ListenAndServe(":"+strconv.Itoa(portNum), nil)
+}
+
+func getLocalIP() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		log.Fatal("Oops: " + err.Error() + "\n")
+		os.Exit(1)
+	}
+
+	for _, a := range addrs {
+		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String()
+			}
+		}
+	}
+
+	return "localhost"
+}
+
+func ping(w http.ResponseWriter, r *http.Request) {
+	log.Println(r)
 	data := struct {
-		Message string
+		result string
 	}{
-		"Hello, World",
+		"pong",
 	}
 
 	if err := json.NewEncoder(w).Encode(data); err != nil {
@@ -19,9 +73,11 @@ func helloWorld(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func main() {
-	http.Handle("/", http.FileServer(http.Dir("../dist/ayzer-web/")))
+func shutdown() {
+	fmt.Println()
+	log.Println("Shut down...")
 
-	http.HandleFunc("/api/hello", helloWorld)
-	http.ListenAndServe(":8080", nil)
+	// clean up
+
+	fmt.Println("Good bye...")
 }
